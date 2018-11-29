de6ba39e241e07df722472d1b8ae465345d0ec98
Jhon Moreno <jhmoreno@heinsohn.com.co>
2018-11-28 23:15:20 -0500

    Se agregan actividades para imprimir desde el celular hacia la impresora

--- app/src/main/java/com/example/jhonjairo/apprmp/MainActivity.java
+++ app/src/main/java/com/example/jhonjairo/apprmp/MainActivity.java
@@ -1,47 +1,244 @@
 package com.example.jhonjairo.apprmp;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+import java.util.Set;
+import java.util.UUID;
+
+public class MainActivity extends Activity implements Runnable {
+    protected static final String TAG = "TAG";
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+    Button mScan, mPrint, mDisc;
+    private EditText editTextIndicacionesMesero;
+    private TextView textViewPruebaMesero;
+    BluetoothAdapter mBluetoothAdapter;
+    private UUID applicationUUID = UUID
+            .fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private ProgressDialog mBluetoothConnectProgressDialog;
+    private BluetoothSocket mBluetoothSocket;
+    BluetoothDevice mBluetoothDevice;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle mSavedInstanceState) {
+        super.onCreate(mSavedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        editTextIndicacionesMesero = (EditText) findViewById(R.id.editTextPrueba);
+        textViewPruebaMesero = (TextView) findViewById(R.id.textViewPruebaMesero);
+        mScan = (Button) findViewById(R.id.Scan);
+        mScan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View mView) {
+                mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (mBluetoothAdapter == null) {
+                    Toast.makeText(MainActivity.this, "Message1", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (!mBluetoothAdapter.isEnabled()) {
+                        Intent enableBtIntent = new Intent(
+                                BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                        startActivityForResult(enableBtIntent,
+                                REQUEST_ENABLE_BT);
+                    } else {
+                        ListPairedDevices();
+                        Intent connectIntent = new Intent(MainActivity.this,
+                                DeviceListActivity.class);
+                        startActivityForResult(connectIntent,
+                                REQUEST_CONNECT_DEVICE);
+                    }
+                }
+            }
+        });
 
-        Intent intent = getIntent();
+        mPrint = (Button) findViewById(R.id.mPrint);
+        mPrint.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View mView) {
+                Thread t = new Thread() {
+                    public void run() {
+                        try {
+                            OutputStream os = mBluetoothSocket
+                                    .getOutputStream();
+                            String BILL = editTextIndicacionesMesero.getText().toString() + "\n" + textViewPruebaMesero.getText().toString();
 
+                            BILL = BILL + "\n\n ";
+                            os.write(BILL.getBytes());
+
+                            //This is printer specific code you can comment ==== > Start
+
+                            // Setting height
+                            int gs = 29;
+                            os.write(intToByteArray(gs));
+                            int h = 104;
+                            os.write(intToByteArray(h));
+                            int n = 162;
+                            os.write(intToByteArray(n));
+
+                            // Setting Width
+                            int gs_width = 29;
+                            os.write(intToByteArray(gs_width));
+                            int w = 119;
+                            os.write(intToByteArray(w));
+                            int n_width = 2;
+                            os.write(intToByteArray(n_width));
+
+
+                        } catch (Exception e) {
+                            Log.e("MainActivity", "Exe ", e);
+                        }
+                    }
+                };
+                t.start();
+            }
+        });
+
+        mDisc = (Button) findViewById(R.id.dis);
+        mDisc.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View mView) {
+                if (mBluetoothAdapter != null)
+                    mBluetoothAdapter.disable();
+            }
+        });
+
+    }// onCreate
+
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        try {
+            if (mBluetoothSocket != null)
+                mBluetoothSocket.close();
+        } catch (Exception e) {
+            Log.e("Tag", "Exe ", e);
+        }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void onBackPressed() {
+        try {
+            if (mBluetoothSocket != null)
+                mBluetoothSocket.close();
+        } catch (Exception e) {
+            Log.e("Tag", "Exe ", e);
+        }
+        setResult(RESULT_CANCELED);
+        finish();
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onActivityResult(int mRequestCode, int mResultCode,
+                                 Intent mDataIntent) {
+        super.onActivityResult(mRequestCode, mResultCode, mDataIntent);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (mRequestCode) {
+            case REQUEST_CONNECT_DEVICE:
+                if (mResultCode == Activity.RESULT_OK) {
+                    Bundle mExtra = mDataIntent.getExtras();
+                    String mDeviceAddress = mExtra.getString("DeviceAddress");
+                    Log.v(TAG, "Coming incoming address " + mDeviceAddress);
+                    mBluetoothDevice = mBluetoothAdapter
+                            .getRemoteDevice(mDeviceAddress);
+                    mBluetoothConnectProgressDialog = ProgressDialog.show(this,
+                            "Connecting...", mBluetoothDevice.getName() + " : "
+                                    + mBluetoothDevice.getAddress(), true, false);
+                    Thread mBlutoothConnectThread = new Thread(this);
+                    mBlutoothConnectThread.start();
+                    // pairToDevice(mBluetoothDevice); This method is replaced by
+                    // progress dialog with thread
+                }
+                break;
+
+            case REQUEST_ENABLE_BT:
+                if (mResultCode == Activity.RESULT_OK) {
+                    ListPairedDevices();
+                    Intent connectIntent = new Intent(MainActivity.this,
+                            DeviceListActivity.class);
+                    startActivityForResult(connectIntent, REQUEST_CONNECT_DEVICE);
+                } else {
+                    Toast.makeText(MainActivity.this, "Message", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
+    private void ListPairedDevices() {
+        Set<BluetoothDevice> mPairedDevices = mBluetoothAdapter
+                .getBondedDevices();
+        if (mPairedDevices.size() > 0) {
+            for (BluetoothDevice mDevice : mPairedDevices) {
+                Log.v(TAG, "PairedDevices: " + mDevice.getName() + "  "
+                        + mDevice.getAddress());
+            }
+        }
+    }
+
+    public void run() {
+        try {
+            mBluetoothSocket = mBluetoothDevice
+                    .createRfcommSocketToServiceRecord(applicationUUID);
+            mBluetoothAdapter.cancelDiscovery();
+            mBluetoothSocket.connect();
+            mHandler.sendEmptyMessage(0);
+        } catch (IOException eConnectException) {
+            Log.d(TAG, "CouldNotConnectToSocket", eConnectException);
+            closeSocket(mBluetoothSocket);
+            return;
+        }
+    }
+
+    private void closeSocket(BluetoothSocket nOpenSocket) {
+        try {
+            nOpenSocket.close();
+            Log.d(TAG, "SocketClosed");
+        } catch (IOException ex) {
+            Log.d(TAG, "CouldNotCloseSocket");
+        }
+    }
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            mBluetoothConnectProgressDialog.dismiss();
+            Toast.makeText(MainActivity.this, "DeviceConnected", Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    public static byte intToByteArray(int value) {
+        byte[] b = ByteBuffer.allocate(4).putInt(value).array();
+
+        for (int k = 0; k < b.length; k++) {
+            System.out.println("Selva  [" + k + "] = " + "0x"
+                    + UnicodeFormatter.byteToHex(b[k]));
         }
 
-        return super.onOptionsItemSelected(item);
+        return b[3];
     }
+
+    public byte[] sel(int val) {
+        ByteBuffer buffer = ByteBuffer.allocate(2);
+        buffer.putInt(val);
+        buffer.flip();
+        return buffer.array();
+    }
+
 }
